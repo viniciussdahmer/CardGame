03283657c04367f1dbb2636e9ef49e4e831341ce
viniciussdahmer <viniciussteindahmer@gmail.com>
2020-05-23 21:59:58 -0300

    Revert "refactor: minor"
    This reverts commit 1326fda47c3c537c0d258a950764aa856fea47ed.

--- Assets/Scripts/CardZoom.cs
+++ Assets/Scripts/CardZoom.cs
@@ -29,7 +29,7 @@ public class CardZoom : MonoBehaviour
     }
 
     private bool IsAnEnemyCard() {
-        return gameObject.CompareTag("EnemyCard") ? true : false;
+        return gameObject.tag == "EnemyCard" ? true : false;
     }
 
 }
