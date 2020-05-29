3eb9bf356418859191d7c3e36eeeb32d9f5c940f
viniciussdahmer <viniciussteindahmer@gmail.com>
2020-05-28 22:33:23 -0300

    fix(attributes): #17

---
+++ Assets/Scripts/CardVisualAttributes.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardVisualAttributes : MonoBehaviour
+{
+    public Text attackLabel;
+    public Text attackText;
+    public Text lifeLabel;
+    public Text lifeText;
+    
+    void Start()
+    {
+        SetAttributesVisibility();
+    }
+
+    public void EndDrag()
+    {
+        SetAttributesVisibility();
+    }
+    
+    private bool IsParentAnEnemyArea()
+    {
+        return gameObject.transform.parent.CompareTag("EnemyArea");
+    }
+
+    private void SetAttributesVisibility()
+    {
+        if (IsParentAnEnemyArea())
+        {
+            attackLabel.enabled = false;
+            attackText.enabled = false;
+            lifeLabel.enabled = false;
+            lifeText.enabled = false;
+        }
+        else
+        {
+            attackLabel.enabled = true;
+            attackText.enabled = true;
+            lifeLabel.enabled = true;
+            lifeText.enabled = true;
+        }
+    }
+}
