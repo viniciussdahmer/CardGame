aa837f7a918456ee63c763403bcab9d5d4859612
viniciussdahmer <viniciussteindahmer@gmail.com>
2020-05-28 21:35:36 -0300

    refactor(zoom): create zoom method

--- Assets/Scripts/CardZoom.cs
+++ Assets/Scripts/CardZoom.cs
@@ -12,24 +12,32 @@ public class CardZoom : MonoBehaviour
     }
 
     public void OnHoverEnter() {
-        if (!IsAnEnemyCard()) {
-            zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 170), Quaternion.identity);
-            zoomCard.transform.SetParent(Canvas.transform, false);
-            zoomCard.layer = LayerMask.NameToLayer("Zoom");
-
-            RectTransform rect = zoomCard.GetComponent<RectTransform>();
-            rect.sizeDelta = new Vector2(162, 240);
-            
-            // For debug purposes
-            Card test = gameObject.GetComponent<Card>();
-            int attack = test.GetAttack();
-            Debug.Log("Attack = " + attack);
+        if (!IsAnEnemyCard())
+        {
+            ZoomCard();
         }
     }
-
+    
     public void OnHoverExit() {
         Destroy(zoomCard);
     }
+
+    private void ZoomCard()
+    {
+        zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x, Input.mousePosition.y + 170),
+            Quaternion.identity);
+        zoomCard.transform.SetParent(Canvas.transform, false);
+        zoomCard.layer = LayerMask.NameToLayer("Zoom");
+
+        RectTransform rect = zoomCard.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(162, 240);
+
+        // For debug purposes
+        Card test = gameObject.GetComponent<Card>();
+        int attack = test.GetAttack();
+        Debug.Log("Attack = " + attack);
+    }
+
 
     private bool IsAnEnemyCard() {
         return gameObject.CompareTag("EnemyCard") ? true : false;
